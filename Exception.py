0ca2b3dc5066f09f63a78cf7b99cb82c65131edb
Raghu Tiwari <Raghu.learn.007@gmail.com>
2024-05-19 11:55:10 +1000

    Code changes 2

--- Exception.py
+++ Exception.py
@@ -2,3 +2,21 @@ try:
     1/0
 except:
     print("This is error")
+
+
+#### Handling multiple Errors in sequence"
+try:
+    #1/0
+    print(xyz)
+except ZeroDivisionError:
+    print("This is ZeroDivisionError handled")
+except NameError:
+    print("This is name error")
+
+#### multiple Errors in same code block as below"
+
+try:
+    #1/0
+    print(xyz)
+except (ZeroDivisionError,NameError):     #Notice paranthesis and , ;
+    print("This is both erros handled")
